867620176060c91ec1a26a3e91eb63a946ea92ea
satish-313 <pradhansatish53@gmail.com>
2021-03-26 13:14:28 +0530

    added dashboard button in main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 const main = () => {
     return(
         <div>
             <h1 className="center">main</h1>
+            <Link to="/dashboard"><button className="center">dashboard</button></Link>
         </div>
     )
 }
