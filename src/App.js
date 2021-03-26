3ff9410382278c553d2380ae23144bf522c9f27a
satish-313 <pradhansatish53@gmail.com>
2021-03-26 12:50:24 +0530

    remove the logo and write hello world

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>hello world! </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
